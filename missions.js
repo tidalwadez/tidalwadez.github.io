0b103519cb40a283d055653cddb11cf512d1c03e
tidalwadez <45081798+tidalwadez@users.noreply.github.com>
2018-11-15 17:25:36 -0600

    mobile nav

--- missions.js
+++ missions.js
@@ -13,38 +13,16 @@ PAGEBUILDER: reus_cddev_donation_form_js
     console.log("jQuery is not loaded");
 }
 
-// $(".menu-btn a").on("click", function(f) {
-//   $(".menu-btn a").text("ACTIVE!");
-// });
-
   //Mobile Menu
   $('.menu-btn, .mobile-nav a.close-mobile-menu-btn').click(function(){
-    // $('.menu-btn a').toggleClass('menu-active');
     $('.mobile-nav').slideToggle();
   });
 
- $('.mobile-nav .sub-nav-open-close').hover(function () {
-        //slide up all the link lists
-        $('.sub-nav, .sub-sub-nav').slideUp('slow');
-        // $('.sub-nav-open-close').html('&#10133;').parent().removeClass('sub-active');
-        //slide down the link list below the h3 clicked - only if its closed
-        if (!$(this).next().is(':visible')) {
-            $(this).next().slideDown('slow');
-            // $('.sub-nav-open-close').html('&#10133;').parent().removeClass('sub-active');
-            // $(this).html('&#10134').parent().addClass('sub-active');
-        }
-    });
 
-  $('.mobile-nav .sub-sub-nav-open-close').hover(function () {
+ $('.mobile-nav .sub-nav-open-close').click(function () {
         //slide up all the link lists
-        $('.sub-sub-nav').slideUp('slow');
-        // $('.sub-sub-nav-open-close').html('&#10133;').parent().removeClass('sub-sub-active');
-        //slide down the link list below the h3 clicked - only if its closed
-        if (!$(this).next().is(':visible')) {
-            $(this).next().slideDown('slow');
-            // $('.sub-sub-nav-open-close').html('&#10133;').parent().removeClass('sub-sub-active');
-            // $(this).html('&#10134').parent().addClass('sub-sub-active');
-        }
+        $('.sub-nav').slideUp('slow');
+        $(this).find('.main-ul').slideToggle('slow');
     });
 
   //Hacking IE 10
