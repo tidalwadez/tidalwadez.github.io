5e747a0e5bd28d0fc44bfc8695b82edd7637d808
tidalwadez <45081798+tidalwadez@users.noreply.github.com>
2018-11-17 14:31:19 -0600

    11/17 changes

--- missions.js
+++ missions.js
@@ -1,4 +1,11 @@
-console.log("this is on");
+/* ==========================================================================
+MAIN JAVASCRIPT
+LAST UPDATED: 05.23.2018
+AUTHOR: JOEL VIXIE, CHARITY DYNAMICS
+UPDATED BY: LEE JASTER, CHARITY DYNAMICS
+PAGEBUILDER: reus_cddev_donation_form_js
+===========================================================================*/
+ console.log("this is on");
 
  if (jQuery) {  
     console.log("jQuery is loaded");
@@ -56,3 +63,39 @@ console.log("this is on");
             // $(this).html('&#10134').parent().addClass('sub-sub-active');
         }
     });
+
+
+$('.gift-level-container').click(function(){
+  $('.gift-level-container').removeClass('active');
+  $(this).addClass('active');
+});
+
+$('.gift-level-container.gift-amount1').click(function(){
+  var inputValue = $('#inputgift1').val();
+  console.log(inputValue)
+  $('#totalGift').text("$" + inputValue)
+});
+
+$('.gift-level-container.gift-amount2').click(function(){
+  var inputValue = $('#inputgift2').val();
+  console.log(inputValue)
+  $('#totalGift').text("$" + inputValue)
+});
+
+$('.gift-level-container.gift-amount3').click(function(){
+  var inputValue = $('#inputgift3').val();
+  console.log(inputValue)
+  $('#totalGift').text("$" + inputValue)
+});
+
+$('.gift-level-container.gift-amount4').click(function(){
+  var inputValue = $('#inputgift4').val();
+  console.log(inputValue)
+  $('#totalGift').text("$" + inputValue)
+});
+
+$('.gift-level-container.other-field').keypress(function(e) {
+  $('#totalGift').text("$" + e.key)
+});
+
+$('#one-time-giving input[type=radio]:checked').parent('div').addClass('active');
